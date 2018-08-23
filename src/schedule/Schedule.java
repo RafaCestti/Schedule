bbf0fb1fd335f35fa5e800be89e35cf928004b19
Rafael Cestti <rcestti78@gmail.com>
2018-08-23 13:16:46 -0400

    initial commit

---
+++ src/schedule/Schedule.java
@@ -0,0 +1,23 @@
+
+package schedule;
+
+public class Schedule {
+
+    
+    public static void main(String[] args) {
+       String firstName = "Rafa";
+       String lastName = "Cestti";
+       int gradYear = 2020;
+       System.out.println(firstName + " " + lastName + " " +  "is my name. I graduate high school on the year " + gradYear + ".");
+       System.out.println("+ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - +\n" +
+"| 1 |   Chemistry                       | Harold Saavedra   |\n" +
+"| 2 |   AP Computer Science             | Matthew Larrubia  |\n" +
+"| 3 |   Digital Art                     | Peter Santa Maria |\n" +
+"| 4 |   Hon Coll Alg & Trig             | Maria Acevedo     |\n" +
+"| 5 |   Hon Global Studies              | James Dugard      |\n" +
+"| 6 |   American History Accelerated    | Oscar Benitez     | \n" +
+"| 7 |   English 11 Accelerated          | David Ulloa       |\n" +
+"+ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - +")
+    }
+
+}
